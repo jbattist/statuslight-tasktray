23dc96230183069e3564722266053eb5f5f018ad
Joe Battistello <jbattist@microsoft.com>
2021-02-24 14:31:50 -0500

    REST Actions Added

--- statuslight-tasktray.py
+++ statuslight-tasktray.py
@@ -1,6 +1,21 @@
 import os
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction
+import requests
+
+def SendColorUpdate(color):
+    
+    url = "Enter your URL"
+    querystring = {"Enter your URL parameters"}
+
+    payload_tempalte = "{{\n\t\"color\": \"{}\"\n}}"
+    payload = payload_tempalte.format(color)
+    headers = {'content-type': 'application/json'}
+
+    response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
+
+    print(response.text)
+
 
 #Initialize the application
 tray_app = QApplication([])
@@ -19,28 +34,34 @@ tray_menu.setVisible(True)
 menulist_tray = QMenu()
 #Red Button
 action_Red = QAction(QIcon(os.path.join(icon_path, "Red.png")), "Red")
+action_Red.triggered.connect(lambda: SendColorUpdate("FF0000"))
 menulist_tray.addAction(action_Red)
 #Yellow Button
 action_Yellow = QAction(QIcon(os.path.join(icon_path, "Yellow.png")), "Yellow")
+action_Yellow.triggered.connect(lambda: SendColorUpdate("FFFF00"))
 menulist_tray.addAction(action_Yellow)
 #Green Button
 action_Green = QAction(QIcon(os.path.join(icon_path, "Green.png")), "Green")
+action_Green.triggered.connect(lambda: SendColorUpdate("00FF00"))
 menulist_tray.addAction(action_Green)
 #Off Button
 #Green Button
 action_Off = QAction( "Off")
+action_Off.triggered.connect(lambda: SendColorUpdate("000000"))
 menulist_tray.addAction(action_Off)
 
 #add a separtor
 menulist_tray.addSeparator()
 
 #Refresh
-actionHelp = QAction("Refresh")
-menulist_tray.addAction(actionHelp)
+action_Refresh = QAction("Refresh")
+menulist_tray.addAction(action_Refresh)
 
 #Quit the application
-actionQuit = QAction("Quit")
-menulist_tray.addAction(actionQuit)
+action_Quit = QAction("Quit")
+action_Quit.triggered.connect(tray_app.quit)
+menulist_tray.addAction(action_Quit)
+
 
 #add a separator
 menulist_tray.addSeparator()
